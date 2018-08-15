e5dd26cca9d313882f0de284f8ed18ffd56a7619
Christopher Lapish <lapishc@gmail.com>
2018-08-15 16:28:42 -0400

    Add files via upload

--- analysis/rawExtractDrinkingData.m
+++ analysis/rawExtractDrinkingData.m
@@ -1,31 +1,25 @@
-function [data, normData, indices] = rawExtractDrinkingData(filename, numIndexes, numRats)
-%aligns fluid levels to trial time
-%   values in column 5 are used as the start point when trimming the
-%   data to the number of specified indexes
-fileData = csvread(filename);
-clear rawNormData;
-for i = 1:(numRats+1)
-	rawNormData(:,i) = fileData(:,i)./fileData(1,i);
-end
-rawIndices = find(fileData(:,5)>=1);
-indices = rawIndices;
-toDelete = [];
-for i = 2:length(rawIndices)
-	if rawIndices(i)-rawIndices(i-1) > 0.95
-		if rawIndices(i)-rawIndices(i-1) < 1.05
-			toDelete = [toDelete, i];
-        end
-	end
-end
-indices(toDelete)=[];
-data = zeros(numIndexes,4);
-normData = zeros(numIndexes,4);
+function [data, indices] = rawExtractDrinkingData(filePull, filePush, numIndexes, numSensors)
 
-for i = 1:numRats
-    %try
+%% Imports data from the file on the SD card called DATALOG.TXT and aligns
+% the data to the first numSensors button pushes. For example, if you have 
+% 4 sensors (numSensors=4) the data will be aligned to the first four
+% button pushes. Data prior to the button push will be ignored. 
+
+%% Inputs: 
+%  filePull= path and name of the file where dataa is help (DATALOG.TXT)
+%  numIndexes=The time you would like to extract after the botton push in seconds
+%  numSensors= the number of sensors you recorded from on the arduino
+%  filePush= name of the .csv that holds the data in sinlge quotes. Example 'data1.csv'
+
+%% Outputs:
+%  data = the voltage values in a numIndexes(time) x numSensors matrix
+%  indices= the time of the button press relative to when the Arudino was turned on
+
+
+fileData = csvread(filePull);           % imports data
+indices = find(fileData(:,5)==1);       % finds button presses and ignores "double presses"
+data = zeros(numIndexes,4);             % preallocate for speed
+for i = 1:numSensors
         data(:,i)=fileData(indices(i):numIndexes+indices(i)-1,4+1-i);
-        normData(:,i) = rawNormData(indices(i):numIndexes+indices(i)-1,4+1-i);
-    %end
 end
-end
-
+csvwrite(filePush,data);                % exports data to user defined name/path
